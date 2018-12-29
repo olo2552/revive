4f044a0eb673ea6dcd5cd2cad854ffd9c39fc48e
Aleksander Wielgorski <a.wielgorski@protonmail.com>
2018-12-29 19:14:06 +0100

    fix ansible user problems

--- REVIVE.sh
+++ REVIVE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pacman -Syu ansible
-sudo ansible-playbook ~/repos/REVIVE/ansible/playbooks/Revive-playbook.yaml
+sudo pacman -Syu ansible
+ansible-playbook ~/repos/REVIVE/ansible/playbooks/Revive-playbook.yaml --ask-become-pass
