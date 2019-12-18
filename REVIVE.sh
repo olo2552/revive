ca5b4a3517ce118896c0f96ac149801b4136be77
awielgorski <a.wielgorski@protonmail.com>
2019-12-18 18:45:17 +0100

    after tr install

--- REVIVE.sh
+++ REVIVE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo pacman -Syu ansible
-ansible-playbook /home/$SUDO_USER/repos/REVIVE/ansible/playbooks/Revive-playbook.yaml --ask-become-pass
+ansible-playbook /home/$SUDO_USER/repos/REVIVE/ansible/playbooks/Revive-playbook.yaml -vv --ask-become-pass
